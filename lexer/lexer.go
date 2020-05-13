cb1d235d7107da42ead0b5eb85167b6c3ea03a67
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-13 18:45:15 +0900

    :sparkles: Add lexer.go NextToken() newToken()

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "../token"
+
 type Lexer struct {
 	input         string
 	position      int     // 入力における現在の位置（現在の文字を指し示す）
@@ -9,11 +11,12 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 
 }
 
-func (L *Lexer) readChar() {
+func (l *Lexer) readChar() {
 	if l.readPostion >= len(l.input) {
 		l.ch = 0
 	}
@@ -22,4 +25,37 @@ func (L *Lexer) readChar() {
 	}
 	l.positon = l.readPosition
 	l.readPositon += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+
+}
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
