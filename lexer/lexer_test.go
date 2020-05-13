1a2d17000a07ccf9b19f20a9bc0a6a073d51ac28
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-13 18:55:05 +0900

    :pencil2: Fix lexer_test.go

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -10,7 +10,7 @@ func TestNextToken(t *testing.T) {
 
 	tests := []struct {
 		expectedType   token.TokenType
-		expectedLitera string
+		expectedLiteral string
 	}{
 		{token.ASSIGN, "="},
 		{token.PLUS, "+"},
@@ -25,17 +25,17 @@ func TestNextToken(t *testing.T) {
 
 	l := New(input)
 
-	for i, tt := range test {
+	for i, tt := range tests {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tokType)
+				i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expextedLiteral, tok.Litera)
+				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
