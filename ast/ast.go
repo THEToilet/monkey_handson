9bd58b032f309749aab628e36236c7b38cb0eda1
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-14 00:29:27 +0900

    :sparkles: Add LetStatement

--- ast/ast.go
+++ ast/ast.go
@@ -13,3 +13,32 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type Program struct {
+	Statements []Statemant
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+			return p.Statements[0].TokenLiteral()
+	} else {
+			return ""
+	}
+}
+
+type LetStatement struct {
+	Token  token.Token // token.LET トークン
+	Name   *Identifier
+	Value  Expression
+}
+
+func (ls *LetStatement) statementNode()			{}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT トークン
+	Value string
+}
+
+func (i *Identifier) expressionNode()				{}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
