058752a9ad357a5fba71821bc5d35cd01245b7d1
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-14 11:40:55 +0900

    :sparkles: Add some func in parse.go

--- parser/parser.go
+++ parser/parser.go
@@ -52,4 +52,40 @@ func (p *Parser) parserStatement() ast.Statement {
 	}
 }
 
-		
+func (p *Parser) parserLetStatement() *ast.LetStatement {
+	stmt := &astLetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &astIdentifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectePeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: セミコロンにに遭遇するまで指揮を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
