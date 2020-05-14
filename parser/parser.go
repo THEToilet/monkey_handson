adf7fc8973d582e655c6228fb45b6b1d8a738af8
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-14 13:14:01 +0900

    :pencil2: Fix

--- parser/parser.go
+++ parser/parser.go
@@ -101,8 +101,8 @@ func (p *Parser) Errors() []string {
 }
 
 
-func (p *Parser) peekError() []string {
-	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekTOken.Type)
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
 }
 
