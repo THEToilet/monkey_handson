d9303670223a4cfd9449f41d1d3a3d8f99003bff
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-19 15:33:04 +0900

    :sparkles:

--- parser/parser.go
+++ parser/parser.go
@@ -43,6 +43,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// 2つトークンを読み込む。curTokenとpeekTokenの両方がセットされる。
 	p.nextToken()
